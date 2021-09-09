5c4e2c94800e273be7232188a73c001e0bc9db33
Abdulkadir Polat <tr.abdulkadir.polat@gmail.com>
2021-09-09 19:51:03 +0300

    fixes

--- src/Components/GifList.js
+++ src/Components/GifList.js
@@ -1,12 +1,13 @@
 import React from "react";
-const GifList = (props) => {
+import PropTypes from "prop-types";
+function GifList({ giphyData }) {
   return (
     <div className="flex flex-wrap justify-evenly mt-3 mb-12">
-      {props.giphyData.map((giphy) => {
+      {giphyData.map(({ id, images }) => {
         return (
           <img
-            key={giphy.id}
-            src={giphy.images.fixed_height.url}
+            key={id}
+            src={images.fixed_height.url}
             className="transform hover:scale-125 hover:z-20 z-0 w-60 h-60  
             bg-no-repeat bg-cover bg-center p-1 rounded-2xl duration-300 ease-in-out "
           />
@@ -14,6 +15,11 @@ const GifList = (props) => {
       })}
     </div>
   );
+}
+
+GifList.propTypes = {
+  giphyData: PropTypes.array.isRequired,
+  id: PropTypes.number.isRequired
 };
 
 export default GifList;
